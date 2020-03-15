cc9a9b779eb5db6d9079b0ef39009c949f811ca7
wood-crafter <59172319+wood-crafter@users.noreply.github.com>
2020-03-15 07:23:42 +0700

    almostDone

--- hangMan.c
+++ hangMan.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 int checkLetter(char userGuess, char* wordToGuess, char* guessedLetter, int numOfLetter);
 void showPuzzle(char userGuess, char* puzzle, char* wordToGuess);
+int randWordIndex(int* blackList, int wordCount, int playTimes);
 
 int main(){
 	int WORD_SIZE = 10 * sizeof(char);
@@ -11,9 +12,8 @@ int main(){
     char userGuess;
     char guessedLetter[26];
     char fileName[21];
-    int numOfLetter = 0;
     int incorrectNum = 0;
-    char wordToGuess[20];
+    int numOfLetter = 0;
     char puzzle[20];
     int wordCount = 0;
     int wordListSize = 1;
@@ -45,62 +45,61 @@ while(1){
 		}
 	}
 
-	int j = 0;
-	for(;j < wordCount;j++){
-		printf("%s", wordList[j]);
-	}
-	return 0;
-//TODO: rand index from wordList
-//TODO: Add this index to blackList
-
-//Read again
-//if(fscanf(file, "%s", c) == EOF){
-//	printf("End of file");
-//}
-//  	fscanf(file, "%s", wordList);
-
-// TODO: Read the File - Get word
+    int blackList[wordCount];
+    int playTimes;
+    srand(time(0));
   	
   	while(userChoice == 13){
   		toContinue = 1;
   		while(toContinue){
-  			printf("Press Enter to play, press ESC to exit");
+  			printf("Press Enter to play, press ESC to exit\n");
   			userChoice = getch();
   			if (userChoice == 13)
                 toContinue = 0;
             if (userChoice == 27)
                 break;
         }
+        int indexWord = randWordIndex(blackList, wordCount, playTimes);
+        playTimes += 1;
+        int boolen = 1;
+        int k = 0;
+        for(; k < strlen(wordList[indexWord]); k++){
+        	puzzle[k] = '-';
+		}
         
-        //TODO: Let user play
-        printf("Enter your guess: ");
-        userGuess = getch();
-        if(checkLetter(userGuess, wordToGuess, guessedLetter, numOfLetter) == 0){
-        	incorrectNum += 1;
-        	numOfLetter += 1;
-        	guessedLetter[numOfLetter - 1] = userGuess;
+        //TOFIX: HAVE TO GUESS 2 times for 1 same latter
+        while(boolen){
+        	printf("Enter your guess: ");
+            userGuess = getch();
+            printf("%c\n", userGuess);
+            int check = checkLetter(userGuess, wordList[indexWord], guessedLetter, numOfLetter) == 2;
+            if(check == 0){
+        	    incorrectNum += 1;
+        	    numOfLetter += 1;
+        	    guessedLetter[numOfLetter - 1] = userGuess;
         	
-        	printf("Sorry, the letter is NOT in the puzzle\n\n");
-        	printf("You currently have %d incorrect guesses!\n", incorrectNum);
-        	printf("Here is your puzzle:\n");
-        	printf("%s", puzzle);
-		}
+        	    printf("\nSorry, the letter is NOT in the puzzle\n");
+        	    printf("You currently have %d incorrect guesses!\n", incorrectNum);
+        	    printf("Here is your puzzle:\n");
+        	    printf("%s\n", puzzle);
+		    }
 		
-		if(checkLetter(userGuess, wordToGuess, guessedLetter, numOfLetter) == 1){
-            numOfLetter += 1;
-        	guessedLetter[numOfLetter - 1] = userGuess;
+		    if(check == 1){
+                numOfLetter += 1;
+            	guessedLetter[numOfLetter - 1] = userGuess;
             
-            showPuzzle(userGuess, puzzle, wordToGuess);
-		}
+                showPuzzle(userGuess, puzzle, wordList[indexWord]);
+	    	}
 		
-		if(checkLetter(userGuess, wordToGuess, guessedLetter, numOfLetter) == 2){
-			incorrectNum += 1;
-        	printf("Sorry, you have guessed that letter already\n");
-        	printf("Now it count a miss!");
-        	printf("You currently have %d incorrect guesses!\n", incorrectNum);
-        	printf("Here is your puzzle:\n");
-        	printf("%s", puzzle);
+		    if(check == 2){
+		    	incorrectNum += 1;
+            	printf("Sorry, you have guessed that letter already\n");
+            	printf("Now it count a miss!");
+            	printf("You currently have %d incorrect guesses!\n", incorrectNum);
+            	printf("Here is your puzzle:\n");
+            	printf("%s", puzzle);
         	
+	    	}
 		}
 		
 	}
@@ -109,17 +108,36 @@ while(1){
 
 }
 
+int randWordIndex(int* blackList, int wordCount, int playTimes){
+	int random;
+	int i;
+	int boolen = 1;
+	
+	while(boolen){
+		boolen = 0;
+		i = 0;
+		random = rand() % wordCount + 0;
+		for(; i < playTimes + 1; i++){
+			if(random == blackList[i]){
+				boolen = 1;
+			}
+		}	
+	}
+	blackList[playTimes] = random;
+	return random;
+}
+
 int checkLetter(char userGuess, char* wordToGuess, char* guessedLetter, int numOfLetter) {
-	int wordLength = strlen(wordToGuess);
 	int i = 0;
 	
-	for(; i < numOfLetter; i++){
+	for(; i < numOfLetter ; i++){
 		if(userGuess == guessedLetter[i]){
 			return 2;
 		}
 	}
 	
-	for(; i < wordLength; i++){
+	i = 0;
+	for(; i < 11; i++){
 		if(userGuess == wordToGuess[i]){
 			return 1;
 		}
@@ -130,15 +148,14 @@ int checkLetter(char userGuess, char* wordToGuess, char* guessedLetter, int numO
 
 //TOFIX: inform
 void showPuzzle(char userGuess, char* puzzle, char* wordToGuess){
-	int wordLength = strlen(wordToGuess);
 	int i = 0;
 	
-	for(; i < wordLength; i++){
-		if(userGuess = wordToGuess[i]){
+	for(; i < 11; i++){
+		if(userGuess == wordToGuess[i]){
 			puzzle[i] = wordToGuess[i];
 		}
 	}
-    printf("Here is your puzzle:\n");
-    printf("%s", puzzle);
+    printf("\nHere is your puzzle:\n");
+    printf("%s\n", puzzle);
 }
 
